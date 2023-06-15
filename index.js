f5dac89105eae7ee6fc16c1baf6849bb7ee1e3e8
Replit user <>
2023-06-15 17:15:14 -0300

    development

--- index.js
+++ index.js
@@ -1,20 +1,34 @@
-var express = require('express');
-var cors = require('cors');
+const express = require('express');
+const cors = require('cors');
+const formidable = require('formidable');
 require('dotenv').config()
 
-var app = express();
+const app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
-app.get('/', function (req, res) {
+app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
-
+app.post('/api/fileanalyse', (req, res) => {
+  const form = formidable({ multiples: true });
+  form.parse(req, (err, fields, files) => {
+    if (err) {
+      console.log(err);
+      return next(err)
+    };
+    // files contains the uploaded file form property (upfile)
+    res.json({
+      name: files.upfile.originalFilename,
+      type: files.upfile.mimetype,
+      size: files.upfile.size,
+    });
+  });
+});
 
 const port = process.env.PORT || 3000;
-app.listen(port, function () {
+app.listen(port, function() {
   console.log('Your app is listening on port ' + port)
 });
